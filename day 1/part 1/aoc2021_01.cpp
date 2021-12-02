12cdd07066fa0057fa5ad534a7108f0044c1222e
kubek344 <66366159+kubek344@users.noreply.github.com>
2021-12-03 00:24:41 +0100

    Create aoc2021_01.cpp

---
+++ day 1/part 1/aoc2021_01.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+int main() {
+    fstream input;
+    input.open("input.txt");
+    string line1;
+    string line2;
+    int output = 0;
+    while (getline(input, line1)){
+        if (line1 > line2 ){
+            output++;
+        }
+        line2 = line1;
+    }
+    cout << output;
+}
